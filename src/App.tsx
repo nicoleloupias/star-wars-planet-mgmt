52d8e1f50d473bcaef5b71004660fcd76ae62961
nicoleloupias <loupiasnicole@gmail.com>
2024-07-04 21:52:34 +0200

    feat: initial router & theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { theme } from "./theme";
 
 function App() {
-  return <ChakraProvider>hello!</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router}></RouterProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
